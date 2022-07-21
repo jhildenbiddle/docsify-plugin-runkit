c70684a2f576c67b21229a7e003599808219e154
John Hildenbiddle <john@hildenbiddle.com>
2022-07-21 11:54:29 -0500

    Update server CDN URL replacement rules

--- server.js
+++ server.js
@@ -39,8 +39,20 @@ browserSync.init({
             replace: '/CHANGELOG.md'
         },
         {
-            match  : /https?:\/\/cdn\.jsdelivr\.net\/npm\/docsify-plugin-runkit[@\d.]*/g,
+            // CDN versioned default
+            // Ex1: //cdn.com/package-name
+            // Ex2: http://cdn.com/package-name@1.0.0
+            // Ex3: https://cdn.com/package-name@latest
+            match  : /(?:https?:)*\/\/.*docsify-plugin-runkit[@\d.latest]*(?=["'])/g,
             replace: '/docsify-plugin-runkit.min.js'
+        },
+        {
+            // CDN paths to local paths
+            // Ex1: //cdn.com/package-name/path/file.js => /path/file.js
+            // Ex2: http://cdn.com/package-name@1.0.0/path/file.js => /path/file.js
+            // Ex3: https://cdn.com/package-name@latest/path/file.js => /path/file.js
+            match  : /(?:https?:)*\/\/.*docsify-plugin-runkit[@\d.latest]*(?=\/)/g,
+            replace: ''
         }
     ]
 });
